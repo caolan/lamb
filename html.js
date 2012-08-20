e970fac4d0a53f9ebd186c3dbb5c4504ce04583d
Caolan McMahon <caolan@caolanmcmahon.com>
2012-08-19 22:47:08 -0700

    sanitize html input when stringifying

--- html.js
+++ html.js
@@ -12,20 +12,33 @@ html.children   = flip(get)(2)
 
 
 html.stringifyAttr = function (k, v) {
-    return k + '="' + v + '"';
+    return html.h(k) + '="' + html.h(v) + '"';
 };
 html.stringifyAttrs = function (attrs) {
     return join(' ', map(apply(html.stringifyAttr), pairs(attrs)) );
 };
 html.stringify = function (el) {
     if (isString(el)) {
-        return el;
+        return html.h(el);
     }
-    return '<' + el[0] + ' ' + html.stringifyAttrs(el[1]) + '>' +
-        concatMap(html.stringify, isArray(el[2]) && el[2] || [el[2]]) +
-    '</' + el[0] + '>';
+    var tag = html.h(el[0]),
+        attrs = html.stringifyAttrs(el[1]),
+        children = isArray(el[2]) ? el[2]: [el[2]];
+
+    return '<' + tag + ' ' + attrs + '>' +
+        concatMap(html.stringify, children) +
+    '</' + tag + '>';
 };
 
+html.escapeHtml = html.h = function (s) {
+    s = ('' + s); /* Coerce to string */
+    s = s.replace(/&/g, '&amp;');
+    s = s.replace(/</g, '&lt;');
+    s = s.replace(/>/g, '&gt;');
+    s = s.replace(/"/g, '&quot;');
+    s = s.replace(/'/g, '&#39;');
+    return s;
+};
 
 if (typeof module !== 'undefined' && module.exports) {
     module.exports = html;
--- test-html.js
+++ test-html.js
@@ -1,20 +1,6 @@
 var html = require('./html');
 
 
-exports['stringify'] = function (test) {
-    var li = ['li', {'class': 'product'}, [
-        ['span', {'class': 'name'}, 'foo'],
-        ['span', {'class': 'price'}, '1.00']
-    ]];
-    var str = html.stringify(li);
-    test.equal(str,
-        '<li class="product">' +
-            '<span class="name">foo</span>' +
-            '<span class="price">1.00</span>' +
-        '</li>');
-    test.done();
-};
-
 exports['tag'] = function (test) {
     test.equal(html.tag(['li', {}, []]), 'li');
     test.equal(html.tag(['div']), 'div');
@@ -39,5 +25,31 @@ exports['children'] = function (test) {
 exports['stringifyAttrs'] = function (test) {
     var a = {'class': 'product', id: 'someproduct'};
     test.equal(html.stringifyAttrs(a), 'class="product" id="someproduct"');
+    var b = {'class': 'product" onclick="foo', id: 'someproduct'};
+    test.equal(
+        html.stringifyAttrs(b),
+        'class="product&quot; onclick=&quot;foo" id="someproduct"'
+    );
+    test.done();
+};
+
+exports['stringify'] = function (test) {
+    var li = ['li', {'class': 'product'}, [
+        ['span', {'class': 'name'}, 'foo'],
+        ['span', {'class': 'price'}, '1.00']
+    ]];
+    var str = html.stringify(li);
+    test.equal(str,
+        '<li class="product">' +
+            '<span class="name">foo</span>' +
+            '<span class="price">1.00</span>' +
+        '</li>');
+    var div = ['div', {'class': 'foo'}, ["<script>alert('hi');</script>"]];
+    test.equal(
+        html.stringify(div),
+        '<div class="foo">' +
+            '&lt;script&gt;alert(&#39;hi&#39;);&lt;/script&gt;' +
+        '</div>'
+    );
     test.done();
 };
